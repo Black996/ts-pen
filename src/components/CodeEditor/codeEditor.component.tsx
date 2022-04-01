1c8d825c225e813111bbb8329862e03e221cf2d9
black996 <rharasis@yahoo.com>
2022-04-01 09:42:23 +0300

    assemble Code Editor Snippet Component

---
+++ src/components/CodeEditor/codeEditor.component.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import esBuildBundle from "../../bundler";
+import CodeArea from "../../components/CodeArea";
+import Preview from "../../components/Preview";
+
+const CodeEditor: React.FC = () => {
+  const [input, setInput] = React.useState("");
+  const [code, setCode] = React.useState("");
+
+  async function onClick() {
+    const outputCode = await esBuildBundle(input);
+    setCode(outputCode);
+  }
+
+  function onChange(value: string) {
+    setInput(value);
+  }
+
+  return (
+    <div>
+      <h1>Code Transpiler</h1>
+      <CodeArea
+        initialValue='const start = "hello world!";'
+        input={input}
+        onClick={onClick}
+        onChange={onChange}
+      />
+      <Preview code={code} />
+    </div>
+  );
+};
+
+export default CodeEditor;
---
+++ src/components/CodeEditor/index.ts
@@ -0,0 +1,1 @@
+export { default } from "./codeEditor.component";
