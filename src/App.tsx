e6ab287e60d46e1107b79099d726ab4044b96668
black996 <rharasis@yahoo.com>
2022-04-01 09:44:42 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-import esBuildBundle from "./bundler";
-import CodeArea from "./components/CodeArea";
-import Preview from "./components/Preview";
+import CodeEditor from "./components/CodeEditor";
 
-const App = () => {
-  const [input, setInput] = React.useState("");
-  const [code, setCode] = React.useState("");
-
-  async function onClick() {
-    const outputCode = await esBuildBundle(input);
-    setCode(outputCode);
-    if (code) return code;
-  }
-
-  function onChange(value: string) {
-    setInput(value);
-  }
-
+const App: React.FC = () => {
   return (
-    <div>
-      <h1>Code Transpiler</h1>
-      <CodeArea
-        initialValue='const start = "hello world!";'
-        input={input}
-        onClick={onClick}
-        onChange={onChange}
-      />
-      <Preview code={code} />
-    </div>
+    <>
+      <CodeEditor />
+    </>
   );
 };
 
