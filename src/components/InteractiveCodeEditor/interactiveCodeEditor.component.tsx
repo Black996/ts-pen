9af25b19ce15871f1071333c2204c2b4aac14552
black996 <rharasis@yahoo.com>
2022-04-03 22:35:25 +0300

    apply interactive code bundling

--- src/components/InteractiveCodeEditor/interactiveCodeEditor.component.tsx
+++ src/components/InteractiveCodeEditor/interactiveCodeEditor.component.tsx
@@ -9,10 +9,14 @@ const InteractiveCodeEditor: React.FC = () => {
   const [input, setInput] = React.useState("");
   const [code, setCode] = React.useState("");
 
-  async function onClick() {
-    const outputCode = await esBuildBundle(input);
-    setCode(outputCode);
-  }
+  React.useEffect(() => {
+    let timerId = setTimeout(async () => {
+      const outputCode = await esBuildBundle(input);
+      setCode(outputCode);
+    }, 750);
+
+    return () => clearTimeout(timerId);
+  }, [input]);
 
   function onChange(value: string) {
     setInput(value);
@@ -25,7 +29,6 @@ const InteractiveCodeEditor: React.FC = () => {
           <CodeArea
             initialValue='const start = "hello world!";'
             input={input}
-            onClick={onClick}
             onChange={onChange}
           />
         </Resizable>
