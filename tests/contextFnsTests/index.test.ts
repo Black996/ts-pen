aaede766044059af83958d1b17649f8b07b6f082
black996 <rharasis@yahoo.com>
2022-04-17 02:13:46 +0300

    context state initializer func to use across context functions testing

---
+++ tests/contextFnsTests/index.test.ts
@@ -0,0 +1,23 @@
+import {beforeEach} from '@jest/globals';
+import { CellsContextState, Cells} from "../../src/context/cellsContextTypes";
+
+export function initializeState(){
+    let state:CellsContextState;
+    beforeEach(()=>{
+        const cells:Cells = { 
+                "a":{ id:"a", type:"code", content:""}
+                , "b":{ id:"b", type:"markup", content:"m"}
+                , "c":{ id:"c", type:"markup", content:"mark"}
+            }
+
+        const order = ["a","b","c"];
+
+        state = {cells, order};
+    });
+    
+    return () => state;
+}
+
+it("Initialize state object getter", ()=>{
+    expect(1).toBe(1);
+})
