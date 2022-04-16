91f0472eb241911b18b1e4f892c431a80fbad485
black996 <rharasis@yahoo.com>
2022-04-17 02:15:14 +0300

    context insertCell fn test suite

---
+++ tests/contextFnsTests/insertCell.test.ts
@@ -0,0 +1,26 @@
+import { ITInsertCellParams} from "../../src/context/cellsContextTypes";
+import { initializeState } from "./index.test";
+
+function insertCell({cell,state}:ITInsertCellParams){
+    const {order, cells} = state;
+    const {id} = cell;
+
+    return {cells:{...cells,[id]:cell},order:[...order, cell.id]};
+}
+
+describe("insertCell function tests",()=>{
+    const getState = initializeState();
+            
+    it('inserts a new cell in the object of cells and save its order in the order array', () => {
+        const id = "d"; 
+        const state = getState();
+        const {cells, order} = state;
+
+        expect(insertCell({cell:{id, type:"code",content:""}, state})).toEqual(
+            {
+            cells:{...cells, [id]: {id:"d",type:"code", content:""} }
+            , order:[...order,id]
+            } 
+        );
+    });
+})
