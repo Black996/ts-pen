9642820e7108c13f429515cc9f0ee3c9413d797c
black996 <rharasis@yahoo.com>
2022-04-17 02:10:53 +0300

    types for cells context

---
+++ src/context/cellsContextTypes.ts
@@ -0,0 +1,49 @@
+export type Cells = {[key:string]:ICell};
+export type CellsContextState = {cells: Cells, order:string[]};
+
+export interface ICell {
+    id:string;
+    type:"code" | "markup";
+    content: string;
+}
+
+export type Direction = "up" | "down";
+
+export interface ICellsContextManager {
+    insertCell: (cell:ICell)=>void;
+    updateCell: (id: string, content:string)=> void;
+    removeCell: (id:string)=> void;
+    moveCell:(id:string, direction:Direction)=>void;
+}
+
+export type CellsContextManagerFn = ()=> ICellsContextManager;
+
+export interface ICellsContext {
+    // loading: boolean;
+    // error:boolean;
+    cells:{[key:string] : ICell};
+    order: string[];
+    cellsContextManager:CellsContextManagerFn;
+}
+
+export interface ITInsertCellParams {
+    cell: ICell;
+    state: CellsContextState;
+}
+
+export interface ITUpdateCellParams {
+    id: string;
+    content: string;
+    state: CellsContextState;
+}
+
+export interface ITMoveCellParams {
+    id:string;
+    direction:Direction;
+    state: CellsContextState;
+}
+
+export interface ITRemoveCellParams {
+    id:string;
+    state: CellsContextState;
+}
---
+++ src/context/index.ts
@@ -0,0 +1,1 @@
+export {default} from "./CellsContextProvider"
