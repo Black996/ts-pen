d0f22f3e682130288a140fbc16b37562d3cca4b0
black996 <rharasis@yahoo.com>
2022-05-05 19:17:39 +0300

    view Cells List

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import CellList from "./components/CellList";
 import CodeArea from "./components/CodeArea";
 import TextEditor from "./components/TextEditor";
+import CellsContextProvider from "./context/CellsContext/CellsContextProvider";
 
 const App: React.FC = () => {
   return (
     <>
+    <CellsContextProvider>
       {/* <CodeArea /> */}
-      <TextEditor />
+      {/* <TextEditor /> */}
+      <CellList/>
+    </CellsContextProvider>
     </>
   );
 };
