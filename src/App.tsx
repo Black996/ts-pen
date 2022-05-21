e0ff1dc366107885b3a295dcb42f149d026390f6
black996 <rharasis@yahoo.com>
2022-05-21 23:13:56 +0300

    provide BundleContext across the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import CellList from "./components/CellList";
 import CodeArea from "./components/CodeArea";
 import TextEditor from "./components/TextEditor";
+import BundleContextProvider from "./context/BundleContext/BundleContextProvider";
 import CellsContextProvider from "./context/CellsContext/CellsContextProvider";
 
 const App: React.FC = () => {
   return (
     <>
-    <CellsContextProvider>
-      {/* <CodeArea /> */}
-      {/* <TextEditor /> */}
-      <CellList/>
-    </CellsContextProvider>
+      <CellsContextProvider>
+        <BundleContextProvider>
+          {/* <CodeArea /> */}
+          {/* <TextEditor /> */}
+          <CellList />
+        </BundleContextProvider>
+      </CellsContextProvider>
     </>
   );
 };
