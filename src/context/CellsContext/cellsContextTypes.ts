81e7e9dfb6203248e9779dc9979882cbe0a8b7cd
black996 <rharasis@yahoo.com>
2022-05-27 19:06:36 +0300

    add types to CellsReducers

--- src/context/CellsContext/cellsContextTypes.ts
+++ src/context/CellsContext/cellsContextTypes.ts
@@ -1,27 +1,59 @@
-export type Cells = {[key:string]:ICell};
-export type CellsContextState = {cells: Cells, order:string[]};
+import { CellsStoreActions } from "./actionTypes";
+
+export type Cells = { [key: string]: ICell };
+export type CellsContextState = { cells: Cells, order: string[] };
+
+export type CellVariant = "code" | "markup";
+
+
+export type InsertCellAfterPayload = { previousCellId: string | null, cellType: CellVariant };
+export type RemoveCellPayload = { cellId: string };
+export type MoveCellPayload = { cellId: string, direction: Direction };
+export type UpdateCellPayload = { cellId: string, content: string };
+
+interface IInsertCellAfterAction {
+    type: CellsStoreActions.InsertCellAfter;
+    payload: InsertCellAfterPayload;
+}
+
+interface IRemoveCellAction {
+    type: CellsStoreActions.RemoveCell;
+    payload: RemoveCellPayload;
+}
+
+interface IMoveCellAction {
+    type: CellsStoreActions.MoveCell;
+    payload: MoveCellPayload;
+}
+
+interface IUpdateCellAction {
+    type: CellsStoreActions.UpdateCell;
+    payload: UpdateCellPayload;
+}
+
+export type CellsStoreAction = IInsertCellAfterAction | IRemoveCellAction | IUpdateCellAction | IMoveCellAction;
 
 export interface ICell {
-    id:string;
-    type:"code" | "markup";
+    id: string;
+    cellType: CellVariant;
     content: string;
 }
 
 export type Direction = "up" | "down";
 
 export interface ICellsContextManager {
-    updateCell: (id: string, content:string)=> void;
-    removeCell: (id:string)=> void;
-    moveCell:(id:string, direction:Direction)=>void;
-    insertCellAfter:(previousCellId:string | null, cellType:"code" | "markup")=>void;
+    updateCell: (id: string, content: string) => void;
+    removeCell: (id: string) => void;
+    moveCell: (id: string, direction: Direction) => void;
+    insertCellAfter: (previousCellId: string | null, cellType: CellVariant) => void;
 }
 
 export interface ICellsContext {
     // loading: boolean;
     // error:boolean;
-    cells:{[key:string] : ICell};
+    cells: { [key: string]: ICell };
     order: string[];
-    cellsContextManager:ICellsContextManager;
+    cellsContextManager: ICellsContextManager;
 }
 
 export interface ITInsertCellParams {
@@ -36,12 +68,12 @@ export interface ITUpdateCellParams {
 }
 
 export interface ITMoveCellParams {
-    id:string;
-    direction:Direction;
+    id: string;
+    direction: Direction;
     state: CellsContextState;
 }
 
 export interface ITRemoveCellParams {
-    id:string;
+    id: string;
     state: CellsContextState;
 }
