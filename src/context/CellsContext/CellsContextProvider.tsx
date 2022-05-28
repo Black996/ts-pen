ad6e865ffb42e78a247ae466d1a77cf5ed7eb877
black996 <rharasis@yahoo.com>
2022-05-29 00:14:28 +0300

    refactor moveCellAction, change types and naming to adapt with the new approach

--- src/context/CellsContext/CellsContextProvider.tsx
+++ src/context/CellsContext/CellsContextProvider.tsx
@@ -1,21 +1,14 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer } from "react";
 
 import CellsContext from "./CellsContext";
-import { swap } from "../../helpers";
 
-import { ICell, ICellsContextManager, Direction, CellVariant, CellsStoreAction, InsertCellAfterPayload, UpdateCellPayload, RemoveCellPayload } from "./cellsContextTypes";
+import { ICellsActionManager, InsertCellAfterPayload, UpdateCellPayload, RemoveCellPayload, MoveCellPayload } from "./cellsContextTypes";
 import { CellsStoreActions } from "./actionTypes";
-import cellsReducer from "./CellsReducer";
-
-
-
+import CellsReducer from "./CellsReducer";
 
 
 const CellsContextProvider: React.FC = ({ children }) => {
-    const [cellsStore, dispatch] = useReducer<typeof cellsReducer>(cellsReducer, { cells: {}, order: [] })
-
-    // const [cells,setCells] = useState<{[id:string]:ICell}>({})
-    // const [order,setOrder] = useState<string[]>([])
+    const [cellsStore, dispatch] = useReducer<typeof CellsReducer>(CellsReducer, { cells: {}, order: [] })
 
     function insertCellAfterAction(payload: InsertCellAfterPayload) {
         dispatch({ type: CellsStoreActions.InsertCellAfter, payload })
@@ -29,40 +22,22 @@ const CellsContextProvider: React.FC = ({ children }) => {
         dispatch({ type: CellsStoreActions.RemoveCell, payload });
     }
 
-    function moveCell(id: string, direction: Direction) {
-        const orderOfCells = [...order];
-        const lastIndex = (orderOfCells.length - 1);
-        const orderIndex = orderOfCells.findIndex((currentId: string) => currentId === id);
-        const targetIndex = direction == "up" ? orderIndex - 1 : orderIndex + 1;
-
-        if (orderIndex === -1) return console.error("This Cell Doesn't Exist!")
-        if (orderOfCells.length == 0) return console.info("No cells to move around!");
-        if (orderOfCells.length == 1) return console.info("There's only one cell in the collection");
-
-
-        if ((orderIndex == lastIndex) && direction == "down") return console.info("The Cell is already the last cell!")
-        if ((orderIndex == 0) && direction == "up") return console.info("The Cell is already the first cell!")
-
-        if (direction === "up") {
-            swap({ arr: orderOfCells, leftIndex: targetIndex, rightIndex: orderIndex })
-        } else if (direction === "down") {
-            swap({ arr: orderOfCells, leftIndex: orderIndex, rightIndex: targetIndex })
-        }
-        setOrder(orderOfCells);
+    function moveCellAction(payload: MoveCellPayload) {
+        dispatch({ type: CellsStoreActions.MoveCell, payload })
     }
 
-    const cellsContextManager: ICellsContextManager = {
-        moveCell
-        , updateCell
-        , removeCell
-        , insertCellAfter
+    const cellsActionsManager: ICellsActionManager = {
+        moveCellAction
+        , updateCellAction
+        , removeCellAction
+        , insertCellAfterAction
     }
 
 
     return (
-        <CellsContext.Provider value={{ cells, order, cellsContextManager }}>
+        <CellsContext.Provider value={{ cellsStore, cellsActionsManager }}>
             {children}
-        </CellsContext.Provider>
+        </CellsContext.Provider >
     )
 }
 
--- src/context/CellsContext/CellsReducer.ts
+++ src/context/CellsContext/CellsReducer.ts
@@ -1,6 +1,7 @@
-import { CellsStoreAction, CellVariant, ICell, InsertCellAfterPayload, RemoveCellPayload, UpdateCellPayload } from "./cellsContextTypes";
+import { CellsStoreAction, ICell, InsertCellAfterPayload, MoveCellPayload, RemoveCellPayload, UpdateCellPayload } from "./cellsContextTypes";
 import { v4 as uuid } from "uuid";
 import { CellsStoreActions } from "./actionTypes";
+import { swap } from "../../helpers";
 
 interface ICellsState {
     cells: { [id: string]: ICell };
@@ -25,7 +26,7 @@ function updateCell(state: ICellsState, { cellId, content }: UpdateCellPayload) 
         console.info("No Such Cell")
         return state;
     }
-    return { cells: { ...state.cells, [cellId]: { ...state.cells[cellId], content } }, order: state.order }
+    return { cells: { ...state.cells, [cellId]: { ...state.cells[cellId], content } }, order: state.order };
 }
 
 function removeCell(state: ICellsState, { cellId }: RemoveCellPayload) {
@@ -34,23 +35,66 @@ function removeCell(state: ICellsState, { cellId }: RemoveCellPayload) {
         return state;
     }
 
-    state.order.filter((id) => id !== cellId);
+    const orderToRemoveIdx = state.order.findIndex((id) => id == cellId);
+    state.order.splice(orderToRemoveIdx, 1);
+
     delete state.cells[cellId];
 
     return state;
 }
 
 
+function moveCell(state: ICellsState, { cellId, direction }: MoveCellPayload) {
+    if (state.order.length == 1 || state.order.length == 0) {
+        console.info(`Move Not Allowed!
+             That means that theere are no cells to move or that there's only one cell in the collection`
+        );
+        return state;
+    }
+
+    const lastIndex = (state.order.length - 1);
+    const currentIndex = state.order.findIndex((currentId: string) => currentId === cellId);
+    const targetIndex = direction == "up" ? currentIndex - 1 : currentIndex + 1;
+
+    if (currentIndex === -1) {
+        console.error("This Cell Doesn't Exist!")
+        return state;
+    }
+
+    if ((currentIndex == lastIndex) && direction == "down") {
+        console.info("The Cell is already the last cell!");
+        return state;
+    } else if ((currentIndex == 0) && direction == "up") {
+        console.info("The Cell is already the first cell!");
+        return state;
+    }
+
+    if (direction === "up") {
+        swap({ arr: state.order, leftIndex: targetIndex, rightIndex: currentIndex })
+    } else if (direction === "down") {
+        swap({ arr: state.order, leftIndex: currentIndex, rightIndex: targetIndex })
+    }
+    return state;
+}
+
 function CellsReducer(state: ICellsState, action: CellsStoreAction): ICellsState {
     const { type, payload } = action;
 
+    /*  creating a new copy cuz mutating the original object would break the app 
+        To reproduce the bug use the original state instead of the copy.
+        Note that Deep Copying is needed and shallow copying is not enough
+    */
+
+    const stateCopy = { cells: { ...state.cells }, order: [...state.order] };
     switch (type) {
         case CellsStoreActions.InsertCellAfter:
-            return insertCellAfter(state, payload);
+            return insertCellAfter(stateCopy, payload as InsertCellAfterPayload);
         case CellsStoreActions.UpdateCell:
-            return updateCell(state, payload);
+            return updateCell(stateCopy, payload as UpdateCellPayload);
         case CellsStoreActions.RemoveCell:
-            return removeCell(state, payload);
+            return removeCell(stateCopy, payload as RemoveCellPayload);
+        case CellsStoreActions.MoveCell:
+            return moveCell(stateCopy, payload as MoveCellPayload);
         default:
             return state;
     }
