3fde51b669f6f4ec4acbc9de21a99a5d1dcc87c9
black996 <rharasis@yahoo.com>
2022-06-11 19:55:13 +0300

    add serve command to cli module and implement serve fn on local-api module

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,7 @@
-function Server() {
-    console.log("Server Started!!!");
+function serve(port: number, filename: string, dir: string) {
+    console.log("Port is: ", port);
+    console.log("filename is: ", filename);
+    console.log("directory of file is: ", dir);
 }
 
-export default Server;
+export default serve;
