7e64e8a20d2470783608f4e9887cf78580643417
black996 <rharasis@yahoo.com>
2022-06-12 23:09:53 +0300

    add needed deps for local-api and init express server

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,7 +1,11 @@
+import express from "express"
+
 function serve(port: number, filename: string, dir: string) {
-    console.log("Port is: ", port);
-    console.log("filename is: ", filename);
-    console.log("directory of file is: ", dir);
+    const app = express();
+
+    return new Promise<void>((resolve, reject) => {
+        app.listen(port, resolve).on("error", reject);
+    });
 }
 
 export default serve;
