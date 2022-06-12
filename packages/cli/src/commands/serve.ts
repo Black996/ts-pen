de754d6140932d07a1fb683d3ef6283133b14585
black996 <rharasis@yahoo.com>
2022-06-12 23:11:01 +0300

    serve local-api server and add basic error handling

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -5,7 +5,14 @@ import serve from "local-api";
 export const serveCommand = new Command().command('serve [filename]')
     .description("Open a file for editing")
     .option("-p, --port <number>", "port to run server on", "4005")
-    .action((filename = 'notebook.js', options: { port: string }) => {
+    .action(async (filename = 'notebook.js', options: { port: string }) => {
         const dir = path.join(process.cwd(), path.dirname(filename))
-        serve(parseInt(options.port), path.basename(filename), dir)
+        try {
+            await serve(parseInt(options.port), path.basename(filename), dir);
+            console.log(`Server Listening on Port ${options.port}`);
+        } catch (err) {
+            if (err instanceof Error) console.log(err.message);
+        }
+
+
     });
