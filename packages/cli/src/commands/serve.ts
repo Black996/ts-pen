3ba43ae9371173884aa84b1f0f55128a58915a68
black996 <rharasis@yahoo.com>
2022-06-18 00:06:22 +0300

    refactor serve fn, add useProxy flag when in dev env

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -2,13 +2,15 @@ import path from "path";
 import { Command } from "commander";
 import serve from "local-api";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const serveCommand = new Command().command('serve [filename]')
     .description("Open a file for editing")
     .option("-p, --port <number>", "port to run server on", "4005")
     .action(async (filename = 'notebook.js', options: { port: string }) => {
-        const dir = path.join(process.cwd(), path.dirname(filename))
+        const directory = path.join(process.cwd(), path.dirname(filename))
         try {
-            await serve(parseInt(options.port), path.basename(filename), dir);
+            await serve({ port: parseInt(options.port), filename: path.basename(filename), directory, useProxy: !isProduction });
             console.log(`Server Listening on Port ${options.port}`);
         } catch (err) {
             if (err instanceof Error) console.log(err.message);
