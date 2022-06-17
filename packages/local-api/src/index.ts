ac428b79afdb7d7f8413033f0e65dc1f4e2d0444
black996 <rharasis@yahoo.com>
2022-06-18 00:07:27 +0300

    implement proxy when in dev env and serve build files when in production env

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,7 +1,30 @@
-import express from "express"
+import express from "express";
+import path from "path";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
-function serve(port: number, filename: string, dir: string) {
+interface IServeParams {
+    port: number;
+    filename: string;
+    directory: string;
+    useProxy: boolean;
+}
+
+function serve({ port, filename, directory, useProxy }: IServeParams) {
     const app = express();
+
+    if (useProxy) {
+        app.use(createProxyMiddleware({
+            target: "http://localhost:3000/",
+            ws: true,
+            logLevel: "silent"
+        }))
+    } else {
+
+        const pkgIndexPath = require.resolve("local-client/build/index.html");
+        const buildPath = path.dirname(pkgIndexPath);
+
+        app.use(express.static(buildPath));
+    }
 
     return new Promise<void>((resolve, reject) => {
         app.listen(port, resolve).on("error", reject);
